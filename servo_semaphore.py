1e8d53ee63d6376aac56823f9cfaded5b806fbfb
sk8board <96895142+sk8board@users.noreply.github.com>
2025-07-03 15:58:33 -0400

    Update and rename servo_semiphore.py to servo_semaphore.py

--- servo_semaphore.py
+++ servo_semaphore.py
@@ -1,4 +1,4 @@
-# Semiphore operated by servo
+# Semaphore operated by servo
 # Servo code is for Pico
 # Servo is Hitec 32645S HS-645MG High Torque 2BB Metal Gear Servo
 # Pin 2 activates two relays which toggle power and ground to the servo so the servo is powered only when moving
