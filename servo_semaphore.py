80ccd9d4a78ac131d45e407edf21a47af56ae213
sk8board <96895142+sk8board@users.noreply.github.com>
2025-07-03 17:18:05 -0400

    Update servo_semaphore.py

--- servo_semaphore.py
+++ servo_semaphore.py
@@ -6,71 +6,71 @@
 from machine import Pin, PWM
 import time
 
-def setup():
-# servo pulse width time
-    val = high = 2_050_000      # 2,050,000 nanoseconds or 2050 microseconds (red)
-    old = mid = 1_600_000       # time in nanoseconds (yellow)
-    low = 1_150_000             # time in nanoseconds (green)
-    step = pos_step = 450_000   # time in nanoseconds (change to next color)
-    neg_step = pos_step * -1
-    increment = pos_inc 18_000  # time in nanoseconds (servo increment during movement)
-    neg_inc = pos_inc * -1
+class semaphore:
 
-# time intervals    
-    interval = 300_000  #time in miliseconds, 5 minuted equals 300,000
-    prev_time = time.ticks_ms()
+    def __init__(self):
+    # servo pulse width time
+        self.val = self.high = 2_050_000        # 2,050,000 nanoseconds or 2050 microseconds (red)
+        self.old = self.mid = 1_600_000         # time in nanoseconds (yellow)
+        self.low = 1_150_000                    # time in nanoseconds (green)
+        self.step = self.pos_step = 450_000     # time in nanoseconds (change to next color)
+        self.neg_step = self.pos_step * -1
+        self.increment = self.pos_inc = 18_000  # time in nanoseconds (servo increment during movement)
+        self.neg_inc = self.pos_inc * -1
 
-# pin assignment
-    slice_freq = 50 # if servo and LED use the same slice (RP2040), then use slice_freq when assigning servo and LED
-    servo = PWM(Pin(0), freq=slice_freq, duty_ns=mid)
-    LED = PWM(Pin(1), freq=slice_freq, duty_u16=duty_percent(50))
-    relay = Pin(Pin(2), Pin.OUT)
-    relay.off()
+    # time intervals    
+        self.interval = 5000  # time in miliseconds, 5 minutes equals 300,000
+        self.prev_time = time.ticks_ms()
 
-def duty_percent(x):  # convert duty cycle percent to u16
-    y = x / 100 * 65535
-    return y
+    # pin assignment
+        self.slice_freq = 50     # if servo and LED use the same slice (RP2040), then use slice_freq when assigning servo and LED
+        self.servo = PWM(Pin(0), freq=self.slice_freq, duty_ns=self.mid)
+        self.LED = PWM(Pin(1), freq=self.slice_freq, duty_u16=int(self.duty_percent(50)))
+        self.relay = Pin(Pin(2), Pin.OUT)
+        self.relay.off()
 
-def init_servo():   # initialize servo to mid position
-    servo.duty_ns(mid)
-    relay.on()
-    time.sleep(1)
-    relay.off()
-    
-def main():
-    curr_time = time.ticks_ms()
-    
-    if time.ticks_diff(prev_time,curr_time) >= interval:
-        prev_time = curr_time
-        if val == low:
-            step = pos_step  # low --> mid (green to yellow)
-        elif val == high:
-            step = neg_step  # high --> mid (red to yellow)
-        else:  # val equal mid
-            if old == high:  
-                increment = neg_inc  # mid --> low (yellow to green)
+    def duty_percent(self,x):    # convert duty cycle percent to u16
+        y = x / 100 * 65535
+        return y
+
+    def init_servo(self):        # initialize servo to mid position
+        self.servo.duty_ns(self.mid)
+        self.relay.on()
+        time.sleep(1)
+        self.relay.off()
+        
+    def main(self):
+        curr_time = time.ticks_ms()
+        if time.ticks_diff(curr_time,self.prev_time) >= self.interval:
+            self.prev_time = curr_time
+            if self.val == self.low:
+                self.step = self.pos_step  # low --> mid (green to yellow)
+            elif self.val == self.high:
+                self.step = self.neg_step  # high --> mid (red to yellow)
+            else:  # val equal mid
+                if self.old == self.high:  
+                    self.increment = self.neg_inc  # mid --> low (yellow to green)
+                else:
+                    self.increment = self.pos_inc  # mid --> high (yellow to red)
+            
+            self.relay.on()
+            
+            # incremently movement of servo to new color position
+            while self.val != self.old:
+                self.old = self.old + self.increment
+                self.servo.duty_ns(self.old)
+                time.sleep_ms(80)
+            
+            self.relay.off()
+            
+            self.old = self.val
+            
+            if self.val == self.mid:
+                self.val = self.step + self.val # next step is red or green
             else:
-                increment = pos_inc  # mid --> high (yellow to red)
-        
-        relay.on()
-        
-        # incremently movement of servo to new color position
-        while val != old:
-            old = old + increment
-            servo.duty_ns(old)
-            time.sleep_ms(80)
-        
-        relay.off()
-        
-        old = val
-        
-        if val == mid:
-            val = step + val # next step is red or green
-        else:
-            val = mid # next step is to mid position (yellow)
+                self.val = self.mid             # next step is to mid position (yellow)
 
-
-setup()
-init_servo()
+run = semaphore()
+run.init_servo()
 while True:
-    main()
+    run.main()
